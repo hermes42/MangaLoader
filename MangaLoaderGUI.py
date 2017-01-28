a0d02c8caa1046fafef727ac11d5189815e4f4ea
Christian Wichmann <christian@freenono.org>
2017-01-28 22:45:53 +0100

    Cleaned up GUI for MangaLoader and added zipping of CBZ files.

--- MangaLoaderGUI.py
+++ MangaLoaderGUI.py
@@ -2,21 +2,16 @@
 
 import os
 import sys
-#import time
 import logging
 import logging.handlers
 
 from PyQt4 import QtGui
-from PyQt4 import QtCore
 
-#from src import MangaBase
-from gui import viewer
 from gui import loader
-#from src.plugins import MangaFoxPlugin
 
 
 APP_NAME = 'MangaLoaderGUI'
-APP_VERSION = 'v0.1'
+APP_VERSION = 'v0.2'
 
 
 # -------------------------------------------------------------------------------------------------
@@ -35,9 +30,7 @@ def create_logger():
     log_to_screen.setLevel(logging.WARNING)
     logger.addHandler(log_to_screen)
     # add logging to file
-    log_to_file = logging.handlers.RotatingFileHandler(LOG_FILENAME,
-                                                       maxBytes=262144,
-                                                       backupCount=5)
+    log_to_file = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=262144, backupCount=5)
     log_to_file.setLevel(LOG_LEVEL)
     log_to_file.setFormatter(logging.Formatter(LOG_FORMAT))
     logger.addHandler(log_to_file)
@@ -52,48 +45,14 @@ def startGUI():
     logger.info('MangaLoaderGUI started.')
     app = QtGui.QApplication(sys.argv)
     app.setApplicationName(APP_NAME)
-    #app.aboutToQuit.connect(handle_exit)
     main = QtGui.QMainWindow()
     main.setWindowTitle('MangaLoader')
-    #main.setWindowState(QtCore.Qt.WindowMaximized)
-    #v = viewer.ImageView(main)
+    # create main window and show it
     v = loader.LoaderWindow(main)
     main.setCentralWidget(v)
     main.show()
     sys.exit(app.exec_())
-
-    #logger.info('loading Loader')
-    #plugin = MangaFoxPlugin.MangaFoxPlugin()    
-    #loader = MangaBase.Loader(plugin, destDir)
-    #if useRange:
-    #    logger.info('loading chapters ' + str(startChapter) + ' - ' + str(endChapter))
-    #    for i in range(startChapter, endChapter+1):
-    #        loader.handleChapter(mangaName, i)
-    #        if doZip:
-    #            loader.zipChapter(mangaName, i)
-    #else:
-    #    logger.info('loading image ' + str(chapter) + ' / ' + str(image))
-    #    loader.handleImage(mangaName, chapter, image)
-    #    if doZip:
-    #            loader.zipChapter(mangaName, chapter)
-    #end_time = time.time()
-    #logger.debug('end time: %.2f s' %(end_time))
-    #logger.info('elapsed time: %.2f s' %(end_time - start_time))
-    #print(('Elapsed Time: %.2f s' %(end_time - start_time)))
-    
-    #from src.plugins import MangaFoxPlugin
-    #from src.MangaBase import Image
-    #m = MangaFoxPlugin.MangaFoxPlugin()
-    #x = m.getListOfMangas()
-    #for i in x:
-    #    if i.name == 'Coppelion':
-    #        c = m.getListOfChapters(i)
-    #n = MangaBase.Manga('Coppelion')
-    #n.manga_url = 'http://mangafox.me/manga/coppelion/'
-    #c = m.getListOfChapters(n)
-    #i = Image(c[14], 4)
-    #m.getImage(i)
-    logger.info('MangaLoader done')
+    logger.info('MangaLoader done.')
 
 
 if __name__ == '__main__':
--- gui/loader.py
+++ gui/loader.py
@@ -16,6 +16,7 @@ from PyQt4 import QtCore
 
 from src.plugins import MangaFoxPlugin
 from src.plugins import OneMangaPlugin
+from src.plugins import KissMangaPlugin
 from src import MangaBase
 
 
@@ -32,9 +33,7 @@ class LoaderWindow(QtGui.QWidget):
         self.main_gui = parent
         self.manga_store_path = os.getcwd()
         self.plugin = MangaFoxPlugin.MangaFoxPlugin()
-        #self.plugin = OneMangaPlugin.OneMangaPlugin()
         self.manga_list = []
-        self.chapter_count = 1
         self.create_fonts()
         self.setup_ui()
         self.set_signals_and_slots()
@@ -71,24 +70,29 @@ class LoaderWindow(QtGui.QWidget):
         grid.addWidget(QtGui.QLabel('Into directory: '), 2, 0)
         self.directory_button = QtGui.QPushButton(self.manga_store_path)
         grid.addWidget(self.directory_button, 2, 1, 1, 3)
+        # add options
+        grid.addWidget(QtGui.QLabel('Create zipped CBZ files: '), 3, 0)
+        self.do_zip_checkbox = QtGui.QCheckBox() 
+        grid.addWidget(self.do_zip_checkbox, 3, 1, 1, 3)
         # add progressbar
         self.loader_progress = QtGui.QProgressBar()
-        grid.addWidget(self.loader_progress, 3, 0, 1, 4)
+        grid.addWidget(self.loader_progress, 4, 0, 1, 4)
         # add load button
         self.load_button = QtGui.QPushButton('Load...')
         grid.addWidget(self.load_button, 5, 0, QtCore.Qt.AlignLeft)
+        # add show button
+        self.show_button = QtGui.QPushButton('Show...')
+        grid.addWidget(self.show_button, 5, 1, QtCore.Qt.AlignLeft)
         # add quit button
         self.quit_button = QtGui.QPushButton('Quit')
         grid.addWidget(self.quit_button, 5, 3, QtCore.Qt.AlignRight)
-        # setup grid layout
-        #grid.setColumnStretch(0, 1)
-        #grid.setColumnStretch(1, 1)
-        #grid.setColumnStretch(2, 1)
-        #grid.setColumnStretch(3, 1)
         self.setLayout(grid)
 
     def buildMangaComboBox(self):
         self.mangaComboBox = QtGui.QComboBox()
+        completer = QtGui.QCompleter(self)
+        completer.setCompletionMode(QtGui.QCompleter.UnfilteredPopupCompletion) #PopupCompletion
+        self.mangaComboBox.setCompleter(completer)
         self.load_manga_list_from_file()
         return self.mangaComboBox
 
@@ -99,6 +103,7 @@ class LoaderWindow(QtGui.QWidget):
         self.load_button.clicked.connect(self.on_load_manga)
         self.mangaComboBox.currentIndexChanged.connect(self.on_update_chapter_fields)
         self.update_list_button.clicked.connect(self.on_update_manga_list)
+        self.show_button.clicked.connect(self.on_show_manga)
 
     def load_manga_list_from_file(self):
         try:
@@ -131,17 +136,20 @@ class LoaderWindow(QtGui.QWidget):
 
     @QtCore.pyqtSlot()
     def on_load_manga(self):
-        logger.info('Loading Loader...')
-        loader = MangaBase.Loader(self.plugin, self.manga_store_path)
+        # check whether the input fields are valid
         startChapter = self.chapter_begin.value()
         endChapter = self.chapter_end.value()
-        self.chapter_count = endChapter - startChapter
-        logger.info('Loading chapters {} - {}'.format(str(startChapter), str(endChapter)))        
+        if endChapter < startChapter:
+            QtGui.QMessageBox.information(self, 'Error', 'Last chapter is smaller than first chapter!')
+            return
+        logger.info('Loading loader...')
+        loader = MangaBase.Loader(self.plugin, self.manga_store_path)
         # setup progress bar for loading of chapters
         self.loader_progress.setRange(startChapter - 1, endChapter)
         self.loader_progress.setValue(startChapter - 1)
         # enforce event processing
         QtGui.QApplication.processEvents()
+        logger.info('Loading chapters {} - {}'.format(str(startChapter), str(endChapter)))        
         for i in range(startChapter, endChapter + 1):
             # find chapter object
             currentChapter = None
@@ -151,11 +159,12 @@ class LoaderWindow(QtGui.QWidget):
                     break
             if currentChapter:
                 loader.handleChapter2(currentChapter)
+                # set new value for progress bar and enforce event processing
                 self.loader_progress.setValue(i)
-                # enforce event processing
                 QtGui.QApplication.processEvents()
-                #if doZip:
-                #    loader.zipChapter(mangaName, i)
+                if self.do_zip_checkbox.checkState():
+                    loader.zipChapter2(self.mangaComboBox.itemData(self.mangaComboBox.currentIndex()),
+                                       currentChapter)
             else:
                 logger.error('Could not find chapter object!')
 
@@ -179,3 +188,10 @@ class LoaderWindow(QtGui.QWidget):
             self.chapter_begin.setMaximum(maximum)
             self.chapter_end.setMinimum(minimum)
             self.chapter_end.setMaximum(maximum)
+            logger.debug('Found chapter min and max: {} - {}'.format(minimum, maximum))
+
+    @QtCore.pyqtSlot()
+    def on_show_manga(self):
+        logger.error('Show current manga chapter')
+        #main.setWindowState(QtCore.Qt.WindowMaximized)
+        #v = viewer.ImageView(main)
